0dce47eab1c4755003c2ee481ac146a61f7e1dac
Vincent Meijer <meijer.vincent@gmail.com>
2018-08-04 16:05:10 +0200

    Move conversion code to separate service file. Fix getting current path

---
+++ PotreeBatchConverter/PotreeConverterService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace PotreeBatchConverter
+{
+    internal class PotreeConverterService
+    {
+        private readonly string _currentDirectory;
+
+        public PotreeConverterService()
+        {
+            _currentDirectory = GetCurrentDirectory();
+        }
+
+        public void ConvertFilesInDirectory(string inputDirectory)
+        {
+            Console.WriteLine($"Reading all Potree-compatible files from {inputDirectory} and its subfolders...");
+
+            var files = Directory.EnumerateFiles(inputDirectory, "*.*", SearchOption.AllDirectories)
+                .Where(f => f.EndsWith(".las", StringComparison.OrdinalIgnoreCase)
+                            || f.EndsWith(".laz", StringComparison.OrdinalIgnoreCase)
+                            || f.EndsWith(".ptx", StringComparison.OrdinalIgnoreCase)
+                            || f.EndsWith("ply", StringComparison.OrdinalIgnoreCase));
+
+            foreach (var file in files)
+            {
+                RunProcess(file);
+            }
+
+            Console.WriteLine("All done. Press the 'Any' key to exit...");
+            Console.ReadKey();
+        }
+
+        private void RunProcess(string file)
+        {
+            Console.WriteLine($"Processing {file}");
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "CMD.exe",
+                Arguments =
+                    $"/c {_currentDirectory}\\PotreeConverter\\PotreeConverter.exe \"{file}\" -o \"{file}.potree\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            var process = new Process {StartInfo = startInfo};
+
+            process.OutputDataReceived += (s, e) => Console.WriteLine(e.Data);
+            process.ErrorDataReceived += (s, e) => Console.WriteLine(e.Data);
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            Console.WriteLine($"Done pocessing {file}");
+        }
+
+        private static string GetCurrentDirectory()
+        {
+            var currentPath = GetCurrentPath();
+            return new Uri(Path.GetDirectoryName(currentPath)).LocalPath;
+        }
+
+        private static string GetCurrentPath()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+        }
+    }
+}
--- PotreeBatchConverter/Program.cs
+++ PotreeBatchConverter/Program.cs
@@ -1,61 +1,25 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
+using DotNet.Config;
 
 namespace PotreeBatchConverter
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Starting Potree Batch Converter...");
 
-            var currentPath = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
-            var currentDir = new Uri(Path.GetDirectoryName(currentPath)).LocalPath;
-
-            Console.WriteLine($"Reading all Potree-compatible files from {currentDir} and its subfolders...");
-
-            var files = Directory.EnumerateFiles(currentDir, "*.*", SearchOption.AllDirectories)
-                .Where(f => f.EndsWith(".las", StringComparison.OrdinalIgnoreCase)
-                            || f.EndsWith(".laz", StringComparison.OrdinalIgnoreCase)
-                            || f.EndsWith(".ptx", StringComparison.OrdinalIgnoreCase)
-                            || f.EndsWith("ply", StringComparison.OrdinalIgnoreCase));
-
-            foreach (var file in files)
-            {
-                RunProcess(currentDir, file);
-            }
-
-            Console.WriteLine("All done. Press the 'Any' key to exit...");
-            Console.ReadKey();
-
+            var settings = InitializeSettings();
+            var inputDir = settings.InputDirectory;
+            var converterService = new PotreeConverterService();
+            converterService.ConvertFilesInDirectory(inputDir);
         }
 
-        private static void RunProcess(string currentDir, string file)
+        private static Settings InitializeSettings()
         {
-            Console.WriteLine($"Processing {file}");
-
-            var startInfo = new ProcessStartInfo
-            {
-                FileName = "CMD.exe",
-                Arguments = $"/c {currentDir}\\PotreeConverter\\PotreeConverter.exe \"{file}\" -o \"{file}.potree\"",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true
-            };
-
-            var process = new Process { StartInfo = startInfo };
-
-            process.OutputDataReceived += (s, e) => Console.WriteLine(e.Data);
-            process.ErrorDataReceived += (s, e) => Console.WriteLine(e.Data);
-
-            process.Start();
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
-            process.WaitForExit();
-
-            Console.WriteLine($"Done pocessing {file}");
+            var settings = new Settings();
+            AppSettings.GlueOnto(settings, "config.txt");
+            return settings;
         }
     }
 }

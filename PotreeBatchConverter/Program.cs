51a355c08877d36714b0c769e48b9aa36bddb23f
Vincent Meijer <meijer.vincent@gmail.com>
2018-08-04 13:22:20 +0200

    Fix Process, log output and errors

--- PotreeBatchConverter/Program.cs
+++ PotreeBatchConverter/Program.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading;
 
 namespace PotreeBatchConverter
 {
@@ -10,12 +9,14 @@ namespace PotreeBatchConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            var currentPath = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
-            var currentDir = new Uri(Path.GetDirectoryName(currentPath));
-            Console.WriteLine($"Reading all .las files from {currentDir.LocalPath} and its subfolders...");
+            Console.WriteLine("Starting Potree Batch Converter...");
 
-            var files = Directory.EnumerateFiles(currentDir.LocalPath, "*.*", SearchOption.AllDirectories)
+            var currentPath = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            var currentDir = new Uri(Path.GetDirectoryName(currentPath)).LocalPath;
+
+            Console.WriteLine($"Reading all Potree-compatible files from {currentDir} and its subfolders...");
+
+            var files = Directory.EnumerateFiles(currentDir, "*.*", SearchOption.AllDirectories)
                 .Where(f => f.EndsWith(".las", StringComparison.OrdinalIgnoreCase)
                             || f.EndsWith(".laz", StringComparison.OrdinalIgnoreCase)
                             || f.EndsWith(".ptx", StringComparison.OrdinalIgnoreCase)
@@ -23,23 +24,38 @@ namespace PotreeBatchConverter
 
             foreach (var file in files)
             {
-                Console.WriteLine($"Processing {file}");
-                var procInfo = new ProcessStartInfo("PotreeConverter.exe ", $"--help")
-                {
-                    CreateNoWindow = false,
-                    UseShellExecute = true,
-                    WindowStyle = ProcessWindowStyle.Hidden
-                };
-                //--source { file}
-                var procRun = Process.Start(procInfo);
-                Thread.Sleep(3000);
-                procRun.WaitForExit();
-                Console.WriteLine($"Done pocessing {file}");
+                RunProcess(currentDir, file);
             }
 
-            Console.WriteLine("All done.");
+            Console.WriteLine("All done. Press the 'Any' key to exit...");
             Console.ReadKey();
 
+        }
+
+        private static void RunProcess(string currentDir, string file)
+        {
+            Console.WriteLine($"Processing {file}");
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "CMD.exe",
+                Arguments = $"/c {currentDir}\\PotreeConverter\\PotreeConverter.exe \"{file}\" -o \"{file}.potree\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            var process = new Process { StartInfo = startInfo };
+
+            process.OutputDataReceived += (s, e) => Console.WriteLine(e.Data);
+            process.ErrorDataReceived += (s, e) => Console.WriteLine(e.Data);
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            Console.WriteLine($"Done pocessing {file}");
         }
     }
 }

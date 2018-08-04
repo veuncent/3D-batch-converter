1cacbdb5853358b7af6afb0966b4e91c22f17700
Vincent Meijer <meijer.vincent@gmail.com>
2018-08-04 12:04:36 +0200

    Initial script

--- PotreeBatchConverter/Program.cs
+++ PotreeBatchConverter/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace PotreeBatchConverter
 {
@@ -10,6 +10,36 @@ namespace PotreeBatchConverter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            var currentPath = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+            var currentDir = new Uri(Path.GetDirectoryName(currentPath));
+            Console.WriteLine($"Reading all .las files from {currentDir.LocalPath} and its subfolders...");
+
+            var files = Directory.EnumerateFiles(currentDir.LocalPath, "*.*", SearchOption.AllDirectories)
+                .Where(f => f.EndsWith(".las", StringComparison.OrdinalIgnoreCase)
+                            || f.EndsWith(".laz", StringComparison.OrdinalIgnoreCase)
+                            || f.EndsWith(".ptx", StringComparison.OrdinalIgnoreCase)
+                            || f.EndsWith("ply, StringComparison.OrdinalIgnoreCase"));
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Processing {file}");
+                var procInfo = new ProcessStartInfo("PotreeConverter.exe ", $"--help")
+                {
+                    CreateNoWindow = false,
+                    UseShellExecute = true,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                };
+                //--source { file}
+                var procRun = Process.Start(procInfo);
+                Thread.Sleep(3000);
+                procRun.WaitForExit();
+                Console.WriteLine($"Done pocessing {file}");
+            }
+
+            Console.WriteLine("All done.");
+            Console.ReadKey();
+
         }
     }
 }

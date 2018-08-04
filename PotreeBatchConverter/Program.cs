830e140e00550b984e7a24c0501d9569e3696a26
Vincent Meijer <meijer.vincent@gmail.com>
2018-08-04 17:16:21 +0200

    Cleanup, boyscout rule

--- PotreeBatchConverter/Program.cs
+++ PotreeBatchConverter/Program.cs
@@ -8,11 +8,16 @@ namespace PotreeBatchConverter
         private static void Main(string[] args)
         {
             Console.WriteLine("Starting Potree Batch Converter...");
+            Console.WriteLine(Environment.NewLine);
 
             var settings = InitializeSettings();
             var inputDir = settings.InputDirectory;
             var converterService = new PotreeConverterService();
             converterService.ConvertFilesInDirectory(inputDir);
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("All done. Press the 'Any' key to exit...");
+            Console.ReadKey();
         }
 
         private static Settings InitializeSettings()

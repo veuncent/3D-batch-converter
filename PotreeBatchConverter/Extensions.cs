d695542673ce21aaf27235e19fc8858f084613c2
Vincent Meijer <meijer.vincent@gmail.com>
2018-08-19 18:33:50 +0200

    Add enum for TargetSystems (Potree, Nexus)

---
+++ PotreeBatchConverter/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotreeBatchConverter
+{
+    public static class Extensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var type = value.GetType();
+            var name = Enum.GetName(type, value);
+            if (name == null) return null;
+
+            var field = type.GetField(name);
+            if (field == null) return null;
+
+            if (Attribute.GetCustomAttribute(field,
+                typeof(DescriptionAttribute)) is DescriptionAttribute attr)
+            {
+                return attr.Description;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ PotreeBatchConverter/TargetSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace PotreeBatchConverter
+{
+    public enum TargetSystem
+    {
+        NotImplemented = 0,
+        [Description("Potree")]
+        Potree = 1,
+        [Description("Nexus")]
+        Nexus = 2
+    }
+}
